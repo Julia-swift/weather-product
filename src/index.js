fbc248129dd58f6f08a84c7577ef25e7b751e122
Julia-swift <112249413+Julia-swift@users.noreply.github.com>
2022-09-10 16:57:50 +0300

    Forecast javascript

--- src/index.js
+++ src/index.js
@@ -28,6 +28,61 @@ currentDay.innerHTML = `${weekDay}`;
 let currentTime = document.querySelector("#current-time");
 currentTime.innerHTML = `${time}`;
 
+//Forecast Day
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
+//Forecast
+function displayForecast(response) {
+  let forecast = response.data.daily;
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `
+      <div class="col-2">
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+           <img
+              
+              src="http://openweathermap.org/img/wn/${
+                forecastDay.weather[0].icon
+              }@2x.png"
+              alt=""
+              width="54"
+              />
+            <div class="weather-forecast-temperatures">
+              <span class="weather-forecast-temperature-max"> ${Math.round(
+                forecastDay.temp.max
+              )}° </span>
+              <span class="weather-forecast-temperature-min"> ${Math.round(
+                forecastDay.temp.min
+              )}° </span>
+            </div>
+      </div>
+  `;
+    }
+  });
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
+function getForecast(coordinates) {
+  let apiKey = "c95d60a1e3adbeb286133f1ebebc2579";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
+}
+
+
 //Celsius and Fahrenheit
 
 function showFahrenheit(event) {
@@ -45,7 +100,7 @@ function showCelsius(event) {
    celsius.classList.add("active");
   fahrenheit.classList.remove("active");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
-}
+    }
 
 let celsiusTemperature = null;
 
@@ -80,6 +135,7 @@ function weatherCondition(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+  getForecast(response.data.coord);
 }
 
 
